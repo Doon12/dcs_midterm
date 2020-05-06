1d5d235e2673ef234df4b9e7e01f34c223d80eb5
junhan <joonhanyi@kaist.ac.kr>
2020-05-06 16:51:00 +0900

    fix mistakes of EchoServer: add bind

--- client/EchoServer.c
+++ client/EchoServer.c
@@ -30,6 +30,14 @@ int main(int argc, char* argv[]) {
 		if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
 			error("bind() error");
 
+		bzero(&servaddr, sizeof(servaddr));
+		servaddr.sin_family = AF_INET;
+		servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+		servaddr.sin_port = htons(atoi(argv[1]));
+
+		if (bind(listenfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) == -1)
+			error("bind() error");
+
 		if (listen(listenfd, 5) == -1)
 			error("listen() error");
 
