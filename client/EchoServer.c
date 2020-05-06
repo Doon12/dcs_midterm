086768aacf0609114bfec86f2163ebe4a3144491
junhan <joonhanyi@kaist.ac.kr>
2020-05-06 16:57:23 +0900

    fixed mistake of EchoServer: changed error msg (bind() error to socket() error)

--- client/EchoServer.c
+++ client/EchoServer.c
@@ -11,48 +11,48 @@
 #define	BUFSIZE	256
 
 typedef struct file_segment {
-		int len;
-		char buf[BUFSIZE];
+	int len;
+	char buf[BUFSIZE];
 } file_segment;
 
 void error (char* msg) {
-		perror(msg);
-		exit(1);
+	perror(msg);
+	exit(1);
 }
 
 int main(int argc, char* argv[]) {
-		int listenfd, connfd, filefd, len;
-		struct sockaddr_in cliaddr, servaddr;
-		socklen_t clilen;
-		char recv_msg[BUFSIZE];
-		file_segment segment;
+	int listenfd, connfd, filefd, len;
+	struct sockaddr_in cliaddr, servaddr;
+	socklen_t clilen;
+	char recv_msg[BUFSIZE];
+	file_segment segment;
 
-		if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
-			error("bind() error");
+	if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+		error("socket() error");
 
-		bzero(&servaddr, sizeof(servaddr));
-		servaddr.sin_family = AF_INET;
-		servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-		servaddr.sin_port = htons(atoi(argv[1]));
+	bzero(&servaddr, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	servaddr.sin_port = htons(atoi(argv[1]));
 
-		if (bind(listenfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) == -1)
-			error("bind() error");
+	if (bind(listenfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) == -1)
+		error("bind() error");
 
-		if (listen(listenfd, 5) == -1)
-			error("listen() error");
+	if (listen(listenfd, 5) == -1)
+		error("listen() error");
 
-		if ((connfd = accept(listenfd, (struct sockaddr*)&cliaddr, &clilen)) == -1)
-			error("accept() error");
+	if ((connfd = accept(listenfd, (struct sockaddr*)&cliaddr, &clilen)) == -1)
+		error("accept() error");
 
-		while(1){
-				len = read(connfd, recv_msg, sizeof(recv_msg));
+	while(1){
+		len = read(connfd, recv_msg, sizeof(recv_msg));
 
-				printf("Data Received: %s\n", recv_msg);
-				recv_msg[len] = '\0';
-				write(connfd, recv_msg, len);
-		}
+		printf("Data Received: %s\n", recv_msg);
+		recv_msg[len] = '\0';
+		write(connfd, recv_msg, len);
+	}
 
-		close(connfd);
-		close(listenfd);
-		return 0;
+	close(connfd);
+	close(listenfd);
+	return 0;
 }
